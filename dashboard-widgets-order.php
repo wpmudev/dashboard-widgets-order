10ec891d842ee0307d1f5968d283e47a66d8442c
moha <moha@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-04-20 08:09:44 +0000

    - Infinite column capability
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/dashboard-widgets-order@13947 f53fe46e-8c02-459b-9fdd-93e75771b278

--- dashboard-widgets-order.php
+++ dashboard-widgets-order.php
@@ -4,7 +4,7 @@ Plugin Name: Dashboard Widgets Order
 Plugin URI: http://premium.wpmudev.org/project/dashboard-widget-order
 Description: Easily customize the order of widgets on all of your users dashboards... giving more prominence to the widgets *you* want them to see and use
 Author: S H Mohanjith (Incsub), Andrew Billits (Incsub)
-Version: 2.0.4.1
+Version: 2.0.4.2
 Author URI: http://premium.wpmudev.org
 WDP ID: 16
 Network: true
@@ -28,25 +28,32 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+
+$dashboard_widgets_order = array();
+$dashboard_widgets_order['normal'] = array('high' => array(), 'core' => array(), 'default' => array(), 'low' => array());
+$dashboard_widgets_order['side'] = array('high' => array(), 'core' => array(), 'default' => array(), 'low' => array());
+$dashboard_widgets_order['column3'] = array('high' => array(), 'core' => array(), 'default' => array(), 'low' => array());
+$dashboard_widgets_order['column4'] = array('high' => array(), 'core' => array(), 'default' => array(), 'low' => array());
+
 //------------------------------------------------------------------------//
 //---Config---------------------------------------------------------------//
 //------------------------------------------------------------------------//
 //  change the order of the widgets here
-$dashboard_widgets_order_left_column[] = 'dashboard_right_now';
-$dashboard_widgets_order_left_column[] = 'dashboard_recent_comments';
-$dashboard_widgets_order_left_column[] = 'dashboard_incoming_links';
-$dashboard_widgets_order_left_column[] = 'dashboard_plugins';
+$dashboard_widgets_order['normal']['core'][] = 'dashboard_right_now';
+$dashboard_widgets_order['normal']['core'][] = 'dashboard_activity';
+$dashboard_widgets_order['normal']['default'][] = 'dashboard_incoming_links';
 
-$dashboard_widgets_order_right_column[] = 'dashboard_quick_press';
-$dashboard_widgets_order_right_column[] = 'dashboard_recent_drafts';
-$dashboard_widgets_order_right_column[] = 'dashboard_primary';
-$dashboard_widgets_order_right_column[] = 'dashboard_secondary';
+$dashboard_widgets_order['side']['core'][] = 'dashboard_quick_press';
+$dashboard_widgets_order['side']['default'][] = 'dashboard_primary';
 
+$dashboard_widgets_order['column3']['high'][] = 'dashboard_secondary';
+
+$dashboard_widgets_order['column4']['high'][] = 'dashboard_plugins';
 //  Change this if you update the widget order and want all users to have
 //  the new order instead of just new users.
 //  Note that this will overwrite the custom widget order users have
 //  configured.
-$dashboard_widgets_order_hash = 'n9c0u203b7u0338u10zxnm1n';
+$dashboard_widgets_order_hash = 'm9c0u203b9u0338u10zxnm1lm';
 
 //------------------------------------------------------------------------//
 //---Hook-----------------------------------------------------------------//
@@ -57,7 +64,7 @@ add_action('init', 'dashboard_widgets_order');
 //------------------------------------------------------------------------//
 
 function dashboard_widgets_order() {
-	global $wpdb, $user_ID, $dashboard_widgets_order_left_column, $dashboard_widgets_order_right_column, $dashboard_widgets_order_hash, $wp_meta_boxes;
+	global $wpdb, $user_ID, $dashboard_widgets_order, $dashboard_widgets_order, $dashboard_widgets_order_hash, $wp_meta_boxes;
 
 	if ( !is_multisite() )
 		exit( 'The Dashboard Widget Order plugin is only compatible with WordPress Multisite.' );
@@ -69,22 +76,21 @@ function dashboard_widgets_order() {
 
 		if ( $dashboard_widget_order_updated != $dashboard_widgets_order_hash ) {
 
-			$left_column = '';
-			foreach ( $dashboard_widgets_order_left_column as $dashboard_widgets_order_left_column_widget ) {
-				$left_column .= $dashboard_widgets_order_left_column_widget . ',';
-			}
-			$left_column = rtrim($left_column, ',');
-
-			$right_column = '';
-			foreach ( $dashboard_widgets_order_right_column as $dashboard_widgets_order_right_column_widget ) {
-				$right_column .= $dashboard_widgets_order_right_column_widget . ',';
-			}
-			$right_column = rtrim($right_column, ',');
-
 			$dashboard_widget_order = array();
 
-			$dashboard_widget_order['side'] = $right_column;
-			$dashboard_widget_order['normal'] = $left_column;
+			foreach ( $dashboard_widgets_order as $location => $sidebar ) {
+
+				if (!isset($dashboard_widget_order[$location])) {
+					$dashboard_widget_order[$location] = array();
+				}
+
+				foreach (array('high', 'core', 'default', 'low') as $priority) {
+					if (count($sidebar[$priority]) > 0) {
+						$dashboard_widget_order[$location][] = join(',', $sidebar[$priority]);
+					}
+				}
+				$dashboard_widget_order[$location] = join(',', $dashboard_widget_order[$location]);
+			}
 
 			update_user_option($user_ID, 'meta-box-order_dashboard', $dashboard_widget_order, true);
 			update_user_option($user_ID, 'meta-box-order_dashboard_updated', $dashboard_widgets_order_hash, true);
